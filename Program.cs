dd3254c7f3cade57b02f3ea5918daf59b31017bd
jorancoen <jorancoen886@gmail.com>
2025-08-21 16:53:30 +0200

    Refactor code structure for improved readability and maintainability

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using blazor_tutorial.Components;
+using Radzen;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -6,13 +7,15 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+// Add Radzen services
+builder.Services.AddScoped<ThemeService>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
